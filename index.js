7b44376797c2f762e680b2992c4e2ad39b242cb8
wendymackennah <wendymackennah5@gmail.com>
2023-12-14 02:12:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+
+var multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+
+
+var num3 = 14;
+var num4 = 5;
+
+var mod = num3 % num4;
+
+console.log(mod); // Output: 4
+
+
+var num1 = 10;
+var num2 = 15;
+var num3 = 20;
+var num4 = 5;
+
+var max = Math.max(num1, num2, num3, num4);
+
+console.log(max); // Output: 20
+
